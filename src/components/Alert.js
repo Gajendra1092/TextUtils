942699ec2be0d47decc9be989d4ec1542176e50b
Gajendra Nehra <121254700+Gajendra1092@users.noreply.github.com>
2024-09-03 23:14:09 +0530

    Update Alert.js
    Height is given to alert div

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -8,8 +8,9 @@ function Alert(props) {
 //   }
   
   return (
-   props.alert && <div className= {`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
-  <strong>Success :</strong> {props.alert.msg}
+  <div style = {{height:50px}}>
+{props.alert && <div className= {`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+  <strong>Success :</strong> {props.alert.msg}}
   </div>
 
   )
